ccbfc8d91e0a120b4d84733355d24077a6927b22
josmel <josmelnyh89@gmail.com>
2015-11-21 15:07:54 -0500

    ss

---
+++ source/app/Http/Controllers/Client/Controller.php.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use Datatables;
+use App\Http\Requests\Client\FormCustomerRequest;
+use App\Http\Requests\Client\FormContactRequest;
+use App\Http\Requests\Client\FormSettingRequest;
+use App\Models\Customer;
+use App\Models\Day;
+use Illuminate\Http\Request;
+use App\Models\Equipment;
+use App\Models\Setting;
+use App\Models\EquipmentCampania;
+use App\Models\Campania;
+use Auth;
+use Hash;
+
+class EquipmentController extends Controller {
+
+    const NAMEC = 'equipment';
+
+    public function getIndex() {
+        if (Auth::customer()->check()) {
+            $id = Auth::customer()->user()->id;
+            $table = new Customer();
+            if (!empty($id)) {
+                $table = Customer::find($id);
+            }
+            return viewc('client.' . self::NAMEC . '.index', compact('table'));
+        }
+        return redirect('login')->with('messageError', 'Inicie sesion');
+    }
+
+    public function getForm($id) {
+        $idProceso = explode('-', $id);
+        $equipment_id = $idProceso[0];
+        $typeCampania = Campania::where('flagactive', '=', '1')
+                        ->whereCustomerId(Auth::customer()->user()->id)->lists('name', 'id');
+        $typeCampania = [null => 'Por favor seleccione una opción'] + $typeCampania;
+        $table = new EquipmentCampania();
+        if (isset($idProceso[1])) {
+            $table = EquipmentCampania::find($idProceso[1]);
+            $Setting = Setting::whereEquipmentCampaniaId($idProceso[1])->lists('day_id');
+            $table->day_id = $Setting;
+        }
+        $day = Day::all();
+        return viewc('client.' . self::NAMEC . '.form', compact('equipment_id', ['table', 'day']), ['typeCampania' => $typeCampania]);
+    }
+
+    public function postForm(FormSettingRequest $request) {
+        if (!empty($request)) {
+            $data = $request->all();
+            if ($request->id) {
+                $obj = EquipmentCampania::find($request->id);
+                $obj->update($data);
+                Setting::whereEquipmentCampaniaId($obj->id)->forceDelete();
+                $idEquipmentCampania = $request->id;
+            } else {
+                $obj = EquipmentCampania::create($data);
+                $idEquipmentCampania = $obj->id;
+            }
+            foreach ($data['day_id'] as $value) {
+                Setting::create(array(
+                    'equipment_campania_id' => $idEquipmentCampania,
+                    'day_id' => $value,
+                    'flagactive' => 1,
+                ));
+            }
+            return redirect('admclient/' . self::NAMEC . '/detalle-campania/' . $data['equipment_id'])->with('messageSuccess', 'Campaña configurada correctamente');
+        }
+        return redirect('admclient/' . self::NAMEC . '/form/' . $data['equipment_id'])->with('messageError', 'Error al configurar campaña');
+    }
+
+    public function getDetalleCampania($equipment_id = null) {
+        return viewc('client.' . self::NAMEC . '.detalle-campania', ['equipment_id' => $equipment_id]);
+    }
+
+    public function postIndex(FormCustomerRequest $request) {
+        if (!empty($request)) {
+            $data = $request->except('credit');
+//            $data['flagactive'] = $request->get('flagactive', 1);
+            $id = Auth::customer()->user()->id;
+            unset($data['password']);
+            $password = $request->get('password', null);
+            if (!empty($password)) {
+                $data['password'] = Hash::make($request->get('password'));
+            }
+            if ($id) {
+                $obj = Customer::find($id);
+                $obj->update($data);
+            }
+            return redirect('admclient/' . self::NAMEC)->with('messageSuccess', 'Perfil Guardado');
+        }
+        return redirect('admclient')->with('messageError', 'Error al guardar el perfil');
+    }
+
+    public function postContact(FormContactRequest $request) {
+        if (!empty($request)) {
+            $data = $request->all();
+            $data['customer_id'] = Auth::customer()->user()->id;
+            $data['flagactive'] = $request->get('flagactive', 1);
+            if ($request->id) {
+                $obj = Equipment::find($request->id);
+                $obj->update($data);
+            } else {
+                $obj = Equipment::create($data);
+            }
+            return array('msg' => 'ok', 'state' => 1, 'data' => null);
+        }
+        return array('msg' => 'Error al guardar el modelo', 'state' => 0, 'data' => null);
+    }
+
+    public function getListDetalleCampania(Request $request) {
+        $idEquipments = $request->input('equipment_id', null);
+        $table = EquipmentCampania::
+                join('equipments', 'equipment_campania.equipment_id', '=', 'equipments.id')
+                ->join('campania', 'equipment_campania.campania_id', '=', 'campania.id')
+                ->select('equipment_campania.id', 'campania.name as nameCampania', 'equipments.name as nameEquipment')
+                ->Where('equipments.customer_id', '=', Auth::customer()->user()->id)
+                ->Where('equipments.id', '=', $idEquipments)
+        ;
+        $datatable = Datatables::of($table);
+        return $datatable->make(true);
+    }
+
+    public function getList() {
+        $table = Equipment::select(['id', 'name', 'phone', 'cellphone'])
+                ->whereCustomerId(Auth::customer()->user()->id);
+        $datatable = Datatables::of($table)
+                ->addColumn('action', function($table) {
+            return '<a href="' . $table->id . '" class="btn btn-warning">Editar</a>
+                    
+                        <a href="#" data-url="/admclient/' . self::NAMEC . '/delete/' . $table->id . '" class="btn btn-danger action_delete" data-id="' . $table->id . '" >Eliminar</a>';
+        })
+        ;
+        return $datatable->make(true);
+    }
+
+    public function getDelete($id) {
+        $table = null;
+        if (!empty($id)) {
+            $table = Equipment::whereId($id)->whereCustomerId(Auth::customer()->user()->id);
+            $table->delete();
+        }
+        return response()->json(array('msg' => 'ok', 'state' => 1, 'data' => null));
+    }
+
+}
--- source/resources/views/client/welcome/index.blade.php
+++ source/resources/views/client/welcome/index.blade.php
@@ -1,5 +1,55 @@
 @extends('client._layouts.layout')
 @section('content')
+<style>@media all and (max-width: 800px){
+        #contenedor .bloque{
+            display: block !important;  /* Cuando el ancho sea inferior a 800px el elemento será un bloque */
+            width: auto !important;
+        }
+    }
+    #contenedor .bloque{
+        background:  	#20B2AA ;
+        display: inline-block;  /* Es esencial para que se muestren los bloques en línea */
+        height:100px;
+        width: 300px;
+        color: white;
+        size: 1.5em;
+        margin:20px;
+    }
+    #contenedor .bloque p{
+
+        color: white;
+        font-size: 1.8em;
+        margin:35px;
+    }
+
+</style>
 <div id="wrapper">
+
+
+    <div class="container_perfl">
+        <div class="texto_perfil">
+            <ul>
+                
+                  <li class="input_form">{!! Form::button('EQUIPOS',array('type'=>'submit')) !!}</li>
+            </ul>
+
+        </div>
+        <div id="contenedor">
+            <div style="background: #FF4500"	 class="bloque"><p>Activos (250)</p></div>  <div class="bloque"><p>Inactivos (150)</p></div>
+            <div style="background: #191970"	 class="bloque"><p>Bloqueados (250)</p></div>
+        </div>
+        <div class="texto_perfil">
+            <ul>
+                <li class="input_form">{!! Form::button('USUARIOS',array('type'=>'submit')) !!}</li>
+            </ul>
+
+        </div>
+        <div id="contenedor">
+            <div  style="background: 	#87CEFA" class="bloque"><p>Activos (20)</p></div>  <div style="background: #FFA500" class="bloque"><p>Inactivos (40)</p></div>
+            <div style="background: #778899"	 class="bloque"><p>Bloqueados (50)</p></div>
+        </div>
+        
+    </div>     
+
 </div>
 @stop
