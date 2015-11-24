4ae20a35e6eb05b600c5ca0b368afccff533b01e
josmel <josmelnyh89@gmail.com>
2015-11-24 01:26:26 -0500

    s

--- source/resources/views/home/login-user/login.blade.php
+++ source/resources/views/home/login-user/login.blade.php
@@ -1,7 +1,7 @@
     {!!Form::open(array( 'method' => 'get','name'=>'redirect')) !!}
     {!! Form::hidden('username', '' ) !!}
     {!! Form::hidden('password', '' ) !!}
-    {!! Form::hidden('mac', 'd' ) !!}
+    {!! Form::hidden('mac', '' ) !!}
     {!! Form::hidden('identity', '' ) !!}
     {!! Form::close() !!}
  <script language='javascript'>document.redirect.submit();</script>
